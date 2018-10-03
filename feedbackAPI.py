06f4aa79c88dfac7886a49933870a4ba3a8191ba
kanin sakwattanavakin <bu.nakip@gmail.com>
2018-10-03 19:09:52 +0700

    add word token

--- feedbackAPI.py
+++ feedbackAPI.py
@@ -12,12 +12,6 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-configfeed = {
-    "apiKey": "AIzaSyA0FincfVwAgrFIQe2FJjL-rk7JsXpLaZE",
-    "authDomain": "classroomfeedback-57c36.firebaseapp.com",
-    "databaseURL": "https://classroomfeedback-57c36.firebaseio.com",
-    "storageBucket": "classroomfeedback-57c36.appspot.com"
-}
 
 configclass = {
   "apiKey": "AIzaSyB5M0bxNI8kj3enMyPAgaksUVXU8qrNa4M",
@@ -25,16 +19,10 @@ configclass = {
   "databaseURL": "https://classattendence-c4e10.firebaseio.com",
   "storageBucket": "classattendence-c4e10.appspot.com"
 }
-test = {
-  "apiKey": "AIzaSyASoxK5tz7VudMnMd9FL89841FxncG8pHA",
-  "authDomain": "aaaaaaa-afb80.firebaseapp.com",
-  "databaseURL": "https://aaaaaaa-afb80.firebaseio.com",
-  "storageBucket": "aaaaaaa-afb80.appspot.com"
-}
 positive_word = ['สอนเข้าใจ','ไม่ยาก','ชอบ','สอนดี','น่าสนใจ','สอนสนุก','สนุก'
                  ,'เยี่ยม','ดี','เข้าใจ','เข้าใจดี']
 negative_word = ['ง่วง','สอนเร็ว','สอบยาก','ข้อสอบยาก','เสียงเบา','พูดเสียงเบา','จดไม่ทัน'
-                 ,'พูดเร็ว','ไม่ค่อยเข้าใจ','งานเยอะ','ปล่อยช้า','ไม่เข้าใจ','ยาก']
+                 ,'พูดเร็ว','ไม่ค่อยเข้าใจ','งานเยอะ','ปล่อยช้า','ไม่เข้าใจ','ยาก','ไม่ได้ยิน']
 
 @app.route("/")
 def hello():
@@ -44,41 +32,48 @@ def hello():
 @app.route('/start', methods=['POST'])
 def getrequest():
     data = json.loads(request.data.decode())
-    print(data)
-    #firebase = pyrebase.initialize_app(configfeed)
-    #db = firebase.database()
-    firebase2 = pyrebase.initialize_app(configclass)
-    db2 = firebase2.database()
-    word = deepcut.tokenize(data["comment"], custom_dict='C:/custom_dict.txt')
+    firebase = pyrebase.initialize_app(configclass)
+    db = firebase.database()
+    word = deepcut.tokenize(data["comment"], custom_dict='custom_dict.txt')
     cut = [w for w in word if not w in stop.stopword]
-    
+    poslist = []
+    neglist = []
+    neulist = cut.copy()
     poscount = 0
     negcount = 0
     for y in cut:
         for pos in positive_word:
             if y == pos:
                 poscount += 1
+                poslist.append(y)
         for neg in negative_word:
             if y == neg:
                 negcount += 1
+                neglist.append(y)
                 
-
+    for y in poslist:
+        if y in neulist:
+            neulist.remove(y)
+        
+    for y in neglist:
+        if y in neulist:
+            neulist.remove(y)
+                
     if(poscount > negcount):
         label = "positive"
     elif(negcount > poscount):
         label = "negative"
     else:
         label = "neutral"
-        
 
-    length = db2.child("users").child(data["uid"]).child("course").child(data["cid"]).child("Feedback").child(data["attendanceId"]).get()
-    insert = json.dumps({'date': data["date"], 'comment': data["comment"], 'feeling': data["feeling"], 'classLabel': label, 'cut': cut}, ensure_ascii=False)
+    token = {'positive': poslist , 'neutral': neulist, 'negative': neglist}
+    length = db.child("users").child(data["uid"]).child("course").child(data["cid"]).child("Feedback").child(data["attendanceId"]).get()
+    insert = json.dumps({'date': data["date"], 'comment': data["comment"], 'feeling': data["feeling"], 'classLabel': label, 'token': token , 'rating': data["ratingList"]}, ensure_ascii=False)
     if length.val() == None:
         pointer = 0
     else:
         pointer = len(length.val())
-    print(json.loads(insert))
-    db2.child("users").child(data["uid"]).child("course").child(data["cid"]).child("Feedback").child(data["attendanceId"]).child(pointer).set(json.loads(insert))
+    db.child("users").child(data["uid"]).child("course").child(data["cid"]).child("Feedback").child(data["attendanceId"]).child(pointer).set(json.loads(insert))
     return jsonify({'status':'201'}) , 201
 
 
